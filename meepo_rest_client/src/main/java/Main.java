49fae7a7e5af2a3f01eca0241be16af8f1957e92
Simon Samaletdin <simon.samaletdin@gmail.com>
2021-01-26 21:45:06 +0100

    some finishing..

--- meepo_rest_client/src/main/java/Main.java
+++ meepo_rest_client/src/main/java/Main.java
@@ -18,21 +18,13 @@ public class Main {
                     messageBuilder.append(noteApi.getNotes());
                     break;
                 case "2":
-                    System.out.println("Please enter the id for the note you would like to read:");
-                    String readId = scanner.nextLine();
-                    messageBuilder.append(noteApi.getNote(readId));
+                    handleGetNote(noteApi, scanner, messageBuilder);
                     break;
                 case "3":
-                    System.out.println("Please write describe title, description and id, separate by new row:");
-                    String title = scanner.nextLine();
-                    String description = scanner.nextLine();
-                    String updateId = scanner.nextLine();
-                    messageBuilder.append(noteApi.updateNote(title, description, updateId));
+                    handleUpdateNote(noteApi, scanner, messageBuilder);
                     break;
                 case "4":
-                    System.out.println("Please enter what note id you would like to delete:");
-                    String deleteId = scanner.nextLine();
-                    messageBuilder.append(noteApi.deleteNote(deleteId));
+                    handleDeleteNote(noteApi, scanner, messageBuilder);
                     break;
                 case "q":
                     scanner.close();
@@ -43,6 +35,26 @@ public class Main {
             System.out.println(messageBuilder);
         }
 
+    }
+
+    private static void handleGetNote(NoteApi noteApi, Scanner scanner, StringBuilder messageBuilder) throws JsonProcessingException {
+        System.out.println("Please enter the id for the note you would like to read:");
+        String readId = scanner.nextLine();
+        messageBuilder.append(noteApi.getNote(readId));
+    }
+
+    private static void handleUpdateNote(NoteApi noteApi, Scanner scanner, StringBuilder messageBuilder) {
+        System.out.println("Please write describe title, description and id, separate by new row:");
+        String title = scanner.nextLine();
+        String description = scanner.nextLine();
+        String updateId = scanner.nextLine();
+        messageBuilder.append(noteApi.updateNote(title, description, updateId));
+    }
+
+    private static void handleDeleteNote(NoteApi noteApi, Scanner scanner, StringBuilder messageBuilder) {
+        System.out.println("Please enter what note id you would like to delete:");
+        String deleteId = scanner.nextLine();
+        messageBuilder.append(noteApi.deleteNote(deleteId));
     }
 
     private static String displayWelcomeMessage() {
